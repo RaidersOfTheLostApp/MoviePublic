5360449f481da331dd5bf5b28602177b3babdea9
Kelly Whiting <kwhiting@gmail.com>
2017-09-05 14:18:20 -0700

    formatting fixed

--- client/src/components/user/VODSetup.js
+++ client/src/components/user/VODSetup.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Subheader from 'material-ui/Subheader';
 import Toggle from 'material-ui/Toggle';
-// import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 
 class VODSetup extends React.Component {
   constructor(props) {
@@ -10,7 +9,7 @@ class VODSetup extends React.Component {
     };
   }
 
-  handleToggle(e, isInputChecked){
+  handleToggle(e, isInputChecked) {
 
   }
 
@@ -26,7 +25,7 @@ class VODSetup extends React.Component {
           <tbody>
             <tr>
               <td>
-                <img style={{width:"29px"}} src="/assets/netflix.png"/>
+                <img style={{width: '29px'}} src="/assets/netflix.png"/>
               </td>
               <td>
                 <Toggle className='toggle' label='Netflix' onToggle={this.handleToggle.bind(this)}/>
@@ -34,7 +33,7 @@ class VODSetup extends React.Component {
             </tr>
             <tr>
               <td>
-                <img style={{width:"29px"}} src="/assets/hbogo.png"/>
+                <img style={{width: '29px'}} src="/assets/hbogo.png"/>
               </td>
               <td>
                 <Toggle className='toggle' label='HBO GO' onToggle={this.handleToggle.bind(this)} />
@@ -42,7 +41,7 @@ class VODSetup extends React.Component {
             </tr>
             <tr>
               <td>
-                <img style={{width:"29px"}} src="/assets/hulu.png"/>
+                <img style={{width: '29px'}} src="/assets/hulu.png"/>
               </td>
               <td>
                 <Toggle className='toggle' label='Hulu' onToggle={this.handleToggle.bind(this)} />
@@ -50,7 +49,7 @@ class VODSetup extends React.Component {
             </tr>
             <tr>
               <td>
-                <img style={{width:"29px"}} src="/assets/amazon.png"/>
+                <img style={{width: '29px'}} src="/assets/amazon.png"/>
               </td>
               <td>
                 <Toggle className='toggle' label='Amazon Video' onToggle={this.handleToggle.bind(this)} />
