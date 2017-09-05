c9f15d92ca662e860a9b7f0c55bb307bcb7e9c32
artliou <artyliou@gmail.com>
2017-09-05 10:30:34 -0700

    Update

--- db/index.js
+++ db/index.js
@@ -1,23 +1,23 @@
-knconst knex = require('knex')(require('../knexfile'));
+const knex = require('knex')(require('../knexfile'));
 const db = require('bookshelf')(knex);
 
 db.plugin('registry');
 
 module.exports = db;
 
-var pg = require('knex')({
-    client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING,
-    searchPath: 'knex,public'
-  });
+// var pg = require('knex')({
+//     client: 'pg',
+//     connection: process.env.PG_CONNECTION_STRING,
+//     searchPath: 'knex,public'
+//   });
 
-  var knex = require('knex')({
-    client: 'pg',
-    version: '7.2',
-    connection: {
-      host : '127.0.0.1',
-      user : 'your_database_user',
-      password : 'your_database_password',
-      database : 'myapp_test'
-    }
-  });
+//   var knex = require('knex')({
+//     client: 'pg',
+//     version: '7.2',
+//     connection: {
+//       host : '127.0.0.1',
+//       user : 'your_database_user',
+//       password : 'your_database_password',
+//       database : 'myapp_test'
+//     }
+//   });
