ba0314c4fc422004fce90efb5f9709ae0f2be3b8
Kelly Whiting <kwhiting@gmail.com>
2017-09-06 17:16:59 -0700

    udpated port variable

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 'use strict';
 const app = require('./app');
 const db = require('../db');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Movie Master App listening on port 3000!');
+  console.log('Movie Master App listening on port ' + PORT);
 });
