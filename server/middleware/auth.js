a383d0298800fee7ad7e266f7d523ec635cb0193
Kelly Whiting <kwhiting@gmail.com>
2017-09-13 15:41:12 -0700

    support for staging db and redis connection

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ const RedisStore = require('connect-redis')(session);
 const searchDb = require('../../mongodb/db.js');
 var Promise = require('bluebird');
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'development') {
   const redisClient = require('redis').createClient(process.env.REDIS_URL);
   var redisStoreClient = {
     url: process.env.REDIS_URL
