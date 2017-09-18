e641b7bc16154011ce0f4033e1c86d7bf9ed78a6
james <JMCritelli6@Yahoo.com>
2017-09-18 16:53:40 -0700

    We're now getting theater data back and can filter it to only dates that will within the specified ranges

--- server/movieAPIHelpers/gracenote.js
+++ server/movieAPIHelpers/gracenote.js
@@ -1,51 +1,55 @@
-// var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var apiKeys = require('../../config/apiKeys');
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-// const tmdb = {
-//   'api_key': '2a43b2c5a07e8be85e7ec15a2b12ab5c',
-//   'base_uri': 'http://api.themoviedb.org/3',
-//   'images_uri': 'http://image.tmdb.org/t/p',
-//   'timeout': 5000,
-//   call: function(url, params, success, error) {
+const gracenote = {
+  'api_key': 'kew4j86k7c8ckcuv6q3sbbsk',
+  'uri': 'http://data.tmsapi.com/v1.1/movies/showings',
+  'timeout': 5000,
+  call: function(params, success, error) {
 
-//     var params_str = 'api_key=' + tmdb.api_key;
-//     for (var key in params) {
-//       if (params.hasOwnProperty(key)) {
-//         params_str += '&' + key + '=' + encodeURIComponent(params[key]);
-//       }
-//     }
-//     var xhr = new XMLHttpRequest();
-//     xhr.timeout = tmdb.timeout;
-//     xhr.ontimeout = function () {
-//       throw ('Request timed out: ' + url + ' ' + params_str);
-//     };
-//     console.log(tmdb.base_uri + url + '?' + params_str, 'dfdfdfdfd');
-//     xhr.open('GET', tmdb.base_uri + url + '?' + params_str, true);
-//     xhr.setRequestHeader('Accept', 'application/json');
-//     xhr.responseType = 'text';
-//     xhr.onreadystatechange = function () {
-//       if (this.readyState === 4) {
-//         if (this.status === 200) {
-//           if (typeof success == 'function') {
-//             success(JSON.parse(this.responseText));
-//           } else {
-//             throw ('No success, but the request gave results');
-//           }
-//         } else {
-//           if (typeof error == 'function') {
-//             if (this.response !== undefined) {
-//               error(JSON.parse(this.response));
-//             } else {
-//               error(this.response);
-//             }
-//           } else {
-//             throw ('No error callback');
-//           }
-//         }
-//       }
-//     };
-//     xhr.send();
-//   }
-// };
+    var params_str = 'api_key=' + gracenote.api_key;
+    for (var key in params) {
+      if (params.hasOwnProperty(key)) {
+        params_str += '&' + key + '=' + encodeURIComponent(params[key]);
+      }
+    }
+    var xhr = new XMLHttpRequest();
+    xhr.timeout = gracenote.timeout;
+    xhr.ontimeout = function () {
+      throw ('Request timed out: ' + url + ' ' + params_str);
+    };
+    console.log('the URL is ', gracenote.uri + '?' + params_str);
+    xhr.open('GET', gracenote.uri + '?' + params_str, true);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          if (typeof success == 'function') {
+          	console.log('this request worked!');
+          	console.log(xhr.responseText);
+            success(xhr.responseText);
+          } else {
+            throw ('No success, but the request gave results');
+          }
+        } else {
+          if (typeof error == 'function') {
+            if (xhr.responseText !== undefined) {
+              console.log('this request failed!');
+              error(xhr.responseText);
+            } else {
+              console.log('this request failed (again)!');	
+              error(xhr.responseText);
+            }
+          } else {
+            throw ('No error callback');
+          }
+        }
+      }
+    };
+    xhr.send();
+  }
+};
 
-// module.exports = tmdb;
-// // })()
+module.exports = gracenote;
+// })()
