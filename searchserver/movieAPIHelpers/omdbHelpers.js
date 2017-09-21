01cd6e981420ee9ef7d0ff8b666a39f37773b1e3
james <JMCritelli6@Yahoo.com>
2017-09-21 10:35:38 -0700

    Some changes to the cdoe in prep for saving movies the user is following

--- searchserver/movieAPIHelpers/omdbHelpers.js
+++ searchserver/movieAPIHelpers/omdbHelpers.js
@@ -2,7 +2,7 @@ const omdb = require('omdb');
 const request = require('request');
 
 module.exports.searchTitle = (title, cb) => {
-  var query = 'http://www.omdbapi.com/?i=tt3896198&apikey=92fd20be&include_video=true&';
+  var query = 'http://www.omdbapi.com/?t=&apikey=92fd20be&include_video=true&';
   query = query + 't=' + title;
   request(query, (err, res) => {
     if (err) {
